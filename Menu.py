923238efe2ca1a15820130e86ec477d9a24af0ce
Toni Kuosmanen <toni.t.kuosmanen@gmail.com>
2015-10-29 15:39:36 +0200

    Reworked the NewGameMenu widget creation

--- Menu.py
+++ Menu.py
@@ -8,28 +8,31 @@ class Application(tk.Tk):
     
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+        # Locks the window size
         self.resizable(width=False,height=False)
-        
+
+        # Top level frame
         self.container = tk.Frame(self, width=self.size[0], height=self.size[1])        
         self.container.master.title("DOOMSWEEP")
         self.container.grid()
-        self.container.rowconfigure(0)
-        self.container.columnconfigure(0)
+        
         # Contains the frame currently in container
         self.currentFrame = ""
 
         # Dictionary containing the main menu and all frames accessible from main menu
         self.frames = {}
         for F in [MainMenu,NewGameMenu]:
+            # Initializes all frames (master = container, controller = self)
             frame = F(self.container,self)
             self.frames[F] = frame
         
         self.showFrame(MainMenu)
-        
+
+    #Removes current frame and draws frame C        
     def showFrame(self, C):
-        # Removes current frame and draws frame C
         # Checks if a frame is loaded
         if self.currentFrame != "":
+            # If a frame is loaded, removes it from grid
             self.currentFrame.grid_remove()
         self.currentFrame = self.frames[C]
         self.currentFrame.grid()
@@ -60,90 +63,46 @@ class MainMenu(tk.Frame):
         buttonSettings.grid()
         buttonQuit.grid()
 
+# Menu that enables the starting of a new game and setting up options for the game        
 class NewGameMenu(tk.Frame):
+    
+    fields = ["Height", "Width", "Mines"]
 
     def __init__(self,parent,controller):
         tk.Frame.__init__(self, parent)
         self.parent = parent
         self.controller = controller
         self.config(width=controller.size[0], height=controller.size[1])
-        self.inputHeight = 16
-        self.inputWidth = 16
-        self.inputMines = 40
         self.createWidgets()
         
     def createWidgets(self):
-        
-        # Wraps the validateEntry method for use in entry validation
-        validateEntry = self.register(self.validateEntry)
-        
-        # Labels
-        labelTitle = tk.Label(self, text="NEW GAME",padx=10)
-        labelHeight = tk.Label(self, text="Height:")
-        labelWidth = tk.Label(self, text="Width:")
-        labelMines = tk.Label(self, text="Mines")
-        # Buttons
-        buttonAddHeight = tk.Button(self,width=1,height=1,padx=0,pady=0, text="+",
-                                    command=self.changeEntry("height",1))
-        buttonSubHeight = tk.Button(self,width=1,height=1,padx=0,pady=0, text="-",
-                                    command=self.changeEntry("height",-1))
-        buttonAddWidth = tk.Button(self,width=1,height=1,padx=0,pady=0, text="+",
-                                   command=self.changeEntry("width",1))
-        buttonSubWidth = tk.Button(self,width=1,height=1,padx=0,pady=0, text="-",
-                                   command=self.changeEntry("width",-1))
-        buttonAddMines = tk.Button(self,width=1,height=1,padx=0,pady=0, text="+",
-                                   command=self.changeEntry("mines",1))
-        buttonSubMines = tk.Button(self,width=1,height=1,padx=0,pady=0, text="-",
-                                   command=self.changeEntry("mines",-1))
-        buttonBack = tk.Button(self, text="BACK",width=10,
-                               command=lambda: self.controller.showFrame(MainMenu))
-        buttonStart = tk.Button(self,width=10, text="START")
-        # Entries
-        entryHeight = tk.Entry(self,width=5,validate="all",vcmd=(validateEntry,'%S','%P'))
-        entryHeight.insert(0,str(self.inputHeight))
-        entryWidth= tk.Entry(self,width=5)
-        entryWidth.insert(0, str(self.inputWidth))
-        entryMines = tk.Entry(self,width=5)
-        entryMines.insert(0,str(self.inputMines))
-
-        # Grid all widgets
-        labelTitle.grid(row=0, columnspan=3)
-        labelHeight.grid(row=1,rowspan=2)
-        labelWidth.grid(row=3,rowspan=2)
-        labelMines.grid(row=5,rowspan=2)
-
-        entryHeight.grid(row=1,rowspan=2, column=2)
-        entryWidth.grid(row=3,rowspan=2, column=2)
-        entryMines.grid(row=5,rowspan=2, column=2)
-
-        buttonAddHeight.grid(row=1,column=1)
-        buttonSubHeight.grid(row=2,column=1)
-        buttonAddWidth.grid(row=3,column=1)
-        buttonSubWidth.grid(row=4,column=1)
-        buttonAddMines.grid(row=5,column=1)
-        buttonSubMines.grid(row=6,column=1)
-        buttonStart.grid(row=7,columnspan=3)
-        buttonBack.grid(row=8,columnspan=3)
-        
-    def changeEntry(self,target,value):
-        
-        if target == "height":
-            self.inputHeight += value
-            print(self.inputHeight)
+        title = tk.Label(self, text="NEW GAME")
+        title.grid()
+        for field in self.fields:
+            row = tk.Frame(self)
+            label = tk.Label(row, text=field, width=6)
+            addBtn = tk.Button(row, text="+", padx=1, pady=1, width=1, height=1, anchor=tk.CENTER)
+            subBtn = tk.Button(row, text="-", padx=1, pady=1, width=1, height=1)
+            entry = tk.Entry(row, width=10)
+            row.grid()
+            label.grid(row=1, padx=3, rowspan=2, column=1, sticky='e')
+            addBtn.grid(row=1, padx=10, column=2, sticky='w')
+            subBtn.grid(row=2, padx=10,  column=2, sticky='w')
+            entry.grid(row=1, padx=3, rowspan=2, column=3, sticky='e')
             
-        elif target == "width":
-            self.inputWidth += value
-        elif target == "mines":
-            self.inputMines += value
-
-    def validateEntry(self,insert,text):
-        # BROKEN!
-        try:
-            int(insert)
-            if int(text) <= 100:
-                return True
-            else:
-                self.insert(0,"100")
-                return True
-        except :
-            return False
+        backButton = tk.Button(self, text="Back", command=lambda:self.controller.showFrame(MainMenu))
+        backButton.grid()
+        
+        
+    # Command for + and - buttons to change the value in the entry field
+    def changeEntry(self,target,value):
+        # TBD
+        pass
+    
+    # Function for new game menu entry validation. insert is the text to be inserted
+    # to the entry and text is the string in the entry field after insertion
+    def validateEntry(self,insert,text,name):
+        # TBD
+        pass
+        
+            
